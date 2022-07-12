c10c6937e7bd5eb108420de6775c15691a94957c
weishaokang <weishaokang17@163.com>
2022-07-12 15:05:01 +0800

    Initial commit

---
+++ src/main/java/per/wsk/retry/AsyncProducer.java
@@ -0,0 +1,130 @@
+package per.wsk.retry;
+
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendCallback;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class AsyncProducer {
+
+
+    /**
+     * 定义同步消息发送者
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        //新建一个消费者，指定这个消费者属于 pg这个消费者组
+        DefaultMQProducer producer = new DefaultMQProducer("pg");
+        producer.setNamesrvAddr("192.168.247.130:9876");
+
+        // 指定发送失败后不进行重试发送 默认2次
+        producer.setRetryTimesWhenSendAsyncFailed(0);
+        // 指定新创建的Topic的Queue数量为2，默认为4
+//        producer.setDefaultTopicQueueNums(2);
+        //设置发送超时时长是5s，默认3s
+        producer.setSendMsgTimeout(5000);
+
+        producer.start();
+
+        for (int i = 0; i < 100; i++) {
+            byte[] body = ("Hi," + i).getBytes();
+            try {
+                Message msg = new Message("myTopicA", "myTag", body);
+                SendResult sendResult = producer.send(msg);
+                System.out.println(sendResult);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } // end-for
+
+        producer.shutdown();
+    }
+
+    /**
+     * 定义异步消息发送生产者
+     * @throws MQClientException
+     */
+    @Test
+    public void test01() throws MQClientException, InterruptedException {
+        //新建一个消费者，指定这个消费者属于 pg这个消费者组
+        DefaultMQProducer producer = new DefaultMQProducer("pg");
+        producer.setNamesrvAddr("192.168.247.130:9876");
+
+        // 指定异步发送失败后不进行重试发送 默认2次
+        producer.setRetryTimesWhenSendAsyncFailed(0);
+        // 指定新创建的Topic的Queue数量为2，默认为4
+        producer.setDefaultTopicQueueNums(2);
+        //设置发送超时时长是5s，默认3s
+        producer.setSendMsgTimeout(5000);
+
+        producer.start();
+
+        for (int i = 0; i < 100; i++) {
+            byte[] body = ("Hi," + i).getBytes();
+            try {
+                Message msg = new Message("myTopicC", "myTag", body);
+
+                // 异步发送。指定回调
+                producer.send(msg, new SendCallback() {
+                    // 当producer接收到MQ发送来的ACK后,才会触发该回调方法的执行
+                    @Override
+                    public void onSuccess(SendResult sendResult) {
+                        System.out.println(sendResult);
+                    }
+                    @Override
+                    public void onException(Throwable e) {
+                        e.printStackTrace();
+                    }
+                });
+                msg.setKeys("key- " +i);
+                SendResult sendResult = producer.send(msg);
+                System.out.println(sendResult);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } // end-for
+
+        // sleep一会儿
+        // 由于采用的是异步发送，发送者必须等到mq发来的ack（证明连接mq网络是连通的），才发送消息。所以若这里不sleep，直接shutdown，相当于没有
+        // 发送消息就关闭了发送者，这时会报错
+        TimeUnit.SECONDS.sleep(3);
+        producer.shutdown();
+    }
+
+
+    /**
+     * 定义单向消息发送生产者
+     * @throws RemotingException
+     * @throws MQClientException
+     * @throws InterruptedException
+     */
+    @Test
+    public void test02() throws RemotingException, MQClientException, InterruptedException {
+        DefaultMQProducer producer = new DefaultMQProducer("pg");
+        producer.setNamesrvAddr("rocketmqOS:9876");
+        producer.start();
+        for (int i = 0; i < 10; i++) {
+            byte[] body = ("Hi," + i).getBytes();
+            Message msg = new Message("single", "someTag", body);
+            // 单向发送 ,该方法没有返回值，不知道消息是否发送成功，发送出去就不管了
+            producer.sendOneway(msg);
+        }
+        producer.shutdown();
+        System.out.println("producer shutdown");
+    }
+
+
+    @Test
+    public void test03(){
+
+
+    }
+
+
+}
---
+++ src/main/java/per/wsk/retry/SomeConsumer.java
@@ -0,0 +1,59 @@
+package per.wsk.retry;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.common.protocol.heartbeat.MessageModel;
+import org.junit.Test;
+
+import java.util.List;
+
+public class SomeConsumer {
+
+
+    public static void main(String[] args) throws MQClientException {
+        new SomeConsumer().test01();
+    }
+
+
+    @Test
+    public void test01() throws MQClientException {
+        // 定义一个pull消费者
+        // DefaultLitePullConsumer consumer = new DefaultLitePullConsumer("cg");
+        // 定义一个push消费者
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("cg");
+
+        //指定nameServer
+        consumer.setNamesrvAddr("192.168.247.130:9876");
+        // 指定从第一条消息开始消费
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+
+        //指定消费的topic和tag   tag是通配符，表示消费所有的tag
+        consumer.subscribe("myTopicC","*");
+        // 指定采用“广播模式”进行消费，默认为“集群模式”
+//        consumer.setMessageModel(MessageModel.BROADCASTING);
+
+        //注册消息监听器
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            // 一旦broker中有了其订阅的消息,该方法就会被触发执行， 其返回值为当前consumer消费的状态
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> list, ConsumeConcurrentlyContext consumeConcurrentlyContext) {
+                for (MessageExt msg : list) {
+                    //逐条消费消息，这里只是测试，项目中真正用到mq中肯定是处理具体的业务
+                    System.out.println(msg);
+                }
+                // 返回消费状态：消费成功
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+
+        // 开启消费者消费
+        consumer.start();
+        System.out.println("Consumer Started");
+    }
+
+}
